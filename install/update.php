9e203105d8ba1c753d86a5d63e0145ec3a693e21
cuneytsenturk <cneytsenturk@gmail.com>
2015-05-29 11:24:33 +0300

    close #11 change language directory english to en-GB

--- install/update.php
+++ install/update.php
@@ -5,10 +5,19 @@
  * @license		GNU General Public License version 3; see LICENSE.txt
  */
  
+ // Version 1.0.3 changes;
 if(version_compare($version, '1.0.3', '<')) {
     // Delete language english directory
     $this->filesystem->remove(DIR_LANGUAGE . 'english');
     $this->filesystem->remove(DIR_CATALOG . 'language/english');
 	
-	$this->db->query("ALTER TABLE `" . DB_PREFIX . "addon` ADD COLUMN `params` TEXT DEFAULT NULL");
+	// Update language directory name
+	$this->db->query("UPDATE `" . DB_PREFIX . "language` SET `directory` = 'en-GB' WHERE `language_id` = 1;");
+
+	// Add field ('params') Addon table
+	$query = $this->db->query("SHOW COLUMNS FROM `" . DB_PREFIX . "addon`");
+	
+	if(!in_array('params', $query->rows)) {
+		$this->db->query("ALTER TABLE `" . DB_PREFIX . "addon` ADD COLUMN `params` TEXT DEFAULT NULL");
+	}
 }
