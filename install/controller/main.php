74021055887cda98c439370098be19ece8525c2b
OSWorX <info@osworx.net>
2015-06-08 12:59:08 +0200

    Update main.php
    + accidentially deleted 1st line

--- install/controller/main.php
+++ install/controller/main.php
@@ -5,17 +5,18 @@
  * @license	GNU General Public License version 3; see LICENSE.txt
  */
 
-public function index() {
+class ControllerMain extends Controller {
+	public function index() {
 		$this->load->model('main');
 
 		$data = $this->language->all();
 
 		$this->document->setTitle($data['heading_main']);
 
-        $data['requirements'] = $this->model_main->checkRequirements();
+        	$data['requirements'] = $this->model_main->checkRequirements();
 
-        $data['header'] = $this->load->controller('header');
-        $data['footer'] = $this->load->controller('footer');
+        	$data['header'] = $this->load->controller('header');
+        	$data['footer'] = $this->load->controller('footer');
 
 		$this->response->setOutput($this->load->view('main.tpl', $data));
 	}
