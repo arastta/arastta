5f198dc638ec8ceff0c58c61d4308322fc1b44f6
cuneytsenturk <cneytsenturk@gmail.com>
2015-06-16 14:28:10 +0300

    add customizer language file

---
+++ admin/language/en-GB/theme/default.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @package		Arastta eCommerce
+ * @copyright	Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @license		GNU General Public License version 3; see LICENSE.txt
+ */
+
+// Text
+$_['text_general_title']        = 'General';
+$_['text_general_description']  = 'General';
+
+$_['text_colors_title']         = 'Colors';
+$_['text_colors_description']   = 'You can change site colors from following options';
+
+$_['text_images_title']         = 'Images';
+$_['text_images_description']   = 'Images';
+
+$_['text_colors_header_background_label']                       = 'Top Nav Background';
+$_['text_colors_header_background_description']                 = '';
+$_['text_colors_menu_background_label']                         = 'Menu Background';
+$_['text_colors_menu_background_description']                   = '';
+$_['text_colors_menu_label']                                    = 'Menu Separator Color';
+$_['text_colors_menu_description']                              = '';
+$_['text_menu_color_label']                                     = 'Menu Text Color';
+$_['text_menu_color_description']                               = '';
+$_['text_menu_text_hover_color_label']                          = 'Menu Text Hover Color';
+$_['text_menu_text_hover_color_description']                    = '';
+$_['text_menu_dropdown_menu_background_color_label']            = 'Menu Dropdown Color';
+$_['text_menu_dropdown_menu_background_color_description']      = '';
+$_['text_menu_dropdown_menu_item_hover_background_color_label'] = 'Menu Dropdown Item Hover Background Color';
+$_['text_menu_dropdown_menu_item_hover_background_color_description'] = '';
+$_['text_menu_dropdown_menu_item_background_color_label']       = 'Menu Dropdown Item Color';
+$_['text_menu_dropdown_menu_item_background_color_description'] = '';
+$_['text_breadcrumds_background_color_label']                   = 'Breadcrumbs Background';
+$_['text_breadcrumds_background_color_description']             = '';
+$_['text_breadcrumbs_border_color_label']                       = 'Breadcrumbs Border Color';
+$_['text_breadcrumbs_border_color_description']                 = '';
+$_['text_breadcrumbs_border_li_label']                          = 'Breadcrumbs Arrow Border Color';
+$_['text_breadcrumbs_border_li_description']                    = '';
+$_['text_breadcrumbs_text_color_label']                         = 'Breadcrumbs  Text Color';
+$_['text_breadcrumbs_text_color_description']                   = '';
+$_['text_breadcrumbs_text_hover_color_label']                   = 'Breadcrumbs Text Hover Color';
+$_['text_breadcrumbs_text_hover_color_description']             = '';
+$_['text_footer_background_color_label']                        = 'Footer Background';
+$_['text_footer_border_color_label']                            = 'Footer Border Color';
+$_['text_footer_border_size_label']                             = 'Footer Border Size';
+$_['text_footer_color_label']                                   = 'Footer Text Color';
+$_['text_footer_text_color_label']                              = 'Footer Name Text Color';
+$_['text_footer_link_text_color_label']                         = 'Footer Link Text Color';
+$_['text_footer_link_hover_text_color_label']                   = 'Footer Link Text Hover Color';
+$_['text_button_group_first_button_background_label']           = 'Button Group First Child(AddtoCart) Color';
+$_['text_button_group_first_button_hover_background_label']     = 'Button Group First Child(AddtoCart) Hover Color';
+$_['text_button_group_first_border_label']                      = 'Button Group First Child(AddtoCart) Hover Color';
+$_['text_button_group_first_button_border_color_label']         = 'Button Group First Child(AddtoCart) Hover Color';
+$_['text_button_group_first_button_border_hover_color_label']   = 'Button Group First Child(AddtoCart) Hover Color';
+$_['text_button_group_first_button_text_color_label']           = 'Button Group First Child(AddtoCart) Icon(Text) Color';
+$_['text_button_group_first_button_text_hover_color_label']     = 'Button Group First Child(AddtoCart) Hover Color';
+$_['text_button_group_other_button_background_color_label']     = 'Button Group Other Children Color';
+$_['text_button_group_other_button_hover_background_color_label'] = 'Button Group Other Children Hover Color';
+$_['text_button_group_other_button_border_color_label']         = 'Button Group Other Children Border Left Color';
+$_['text_button_group_other_button_text_color_label']           = 'Button Group Other Children Icon(Text) Color';
+$_['text_button_group_other_button_text_hover_color_label']     = 'Button Group Other Children Icon(Text) Hover Color';
+$_['text_primary_button_background_label']                      = 'Primary Button Color';
+$_['text_primary_button_hover_background_label']                = 'Primary Button Hover Color';
+$_['text_primary_button_border_color_label']                    = 'Primary Button Border Color';
+$_['text_primary_button_text_color_label']                      = 'Primary Button Icon(Text) Color';
+$_['text_primary_button_text_hover_color_label']                = 'Primary Button Icon(Text) Hover Color';
+$_['text_danger_button_background_label']                       = 'Danger Button Color';
+$_['text_danger_button_hover_background_label']                 = 'Danger Button Hover Color';
+$_['text_danger_button_border_color_label']                     = 'Danger Button Border Color';
+$_['text_danger_button_text_color_label']                       = 'Danger Button Icon(Text) Color';
+$_['text_danger_button_text_hover_color_label']                 = 'Danger Button Icon(Text) Hover Color';
+$_['text_default_button_background_label']                      = 'Default Button Color';
+$_['text_default_button_hover_background_label']                = 'Default Button Hover Color';
+$_['text_default_button_border_color_label']                    = 'Default Button Border Color';
+$_['text_default_button_border_hover_color_label']              = 'Default Button Border Hover Color';
+$_['text_default_button_text_color_label']                      = 'Default Button Icon(Text) Color';
+$_['text_default_button_text_hover_color_label']                = 'Default Button Icon(Text) Hover Color';
