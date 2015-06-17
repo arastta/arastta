ebc9df4222bd24bed86a381921b6ca9ec5005f38
OSWorX <info@osworx.net>
2015-06-17 18:11:14 +0200

    Update language_override.php
    New and updated vars and strings

--- admin/language/en-GB/system/language_override.php
+++ admin/language/en-GB/system/language_override.php
@@ -6,29 +6,32 @@
  */
 
 // Heading
-$_['heading_title']          = 'Language Overrides';
+$_['heading_title']			= 'Edit Language';
 
 // Text
-$_['text_success']           = 'Success: You have modified language strings!';
-$_['text_list']              = 'String List';
-$_['text_admin']             = 'Admin';
-$_['text_catalog']           = 'Catalog';
+$_['text_success']			= 'Record successfully modified.';
+$_['text_success_saved']	= 'Successfully saved <b>%s</b> item(s).';
+$_['text_list']				= 'String List';
+$_['text_admin']			= 'Admin';
+$_['text_catalog']			= 'Catalog';
+$_['text_all']				= ' - All -';
+
+$_['entry_text']			= 'Text / Variable';
+$_['entry_limit']			= 'Limit';
 
 // Column
-$_['column_client']          = 'Client';
-$_['column_language']        = 'Language';
-$_['column_folder']          = 'Folder';
-$_['column_path']            = 'Path';
-$_['column_constant']        = 'Constant';
-$_['column_text']            = 'Text';
+$_['column_client']			= 'Client';
+$_['column_language']		= 'Language';
+$_['column_folder']			= 'Folder';
+$_['column_path']			= 'File';
+$_['column_constant']		= 'Variable';
+$_['column_text']			= 'Text (Translation)';
 
-// Help
-$_['help_filter']            = '(Autocomplete)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the keyword is globally unique.';
-$_['help_top']               = 'Display in the top menu bar. Only works for the top parent categories.';
-$_['help_column']            = 'Number of columns to use for the bottom 3 categories. Only works for the top parent categories.';
+$_['help_text']				= 'You can enter a text or a variable (whole or parts).<br />Example: text = Cart, variable = text_cart';
+$_['button_show_help']		= 'Hilfe anzeigen';
+$_['button_hide_help']		= 'Hilfe verbergen';
+
+$_['help_common']			= '<p>You can enter a text or a variable (whole or parts).<br />Example: text = <b>Cart</b>, variable = <b>text_cart</b>.</p><p>Experttip: clicking on the entries below will copy them into the formular';
 
 // Error
-$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']       = 'Warning: You do not have permission to modify language strings!';
-$_['error_name']             = 'Language String must be between 2 and 32 characters!';
+$_['error_permission']		= 'You do not have the permission to modify.';
