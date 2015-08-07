8bd5757462f883989f680e58269a4b87e79c20a1
cuneytsenturk <cneytsenturk@gmail.com>
2015-08-07 10:23:03 +0300

    #165 change some language text

--- admin/language/en-GB/feed/facebook_store.php
+++ admin/language/en-GB/feed/facebook_store.php
@@ -17,28 +17,27 @@ $_['tab_products']                   = 'Products';
 
 // Text
 $_['text_edit']                      = 'Facebook Store Edit';
-$_['text_success']                   = 'Success: You have modified module facebook store!';
-$_['text_remove_feed']               = 'Are you sure Feed item remove?';
+$_['text_success']                   = 'Success: You have modified Facebook Store feed!';
+$_['text_remove_feed']               = 'Are you sure you want to remove this item?';
 
 // Entry
-$_['entry_app_id']                   = 'Facebook APP ID';
+$_['entry_app_id']                   = 'Facebook App ID';
 $_['entry_show_header_category']     = 'Show Category';
 $_['entry_show_header_search']       = 'Show Search';
 $_['entry_show_header_currency']     = 'Show Currency';
 $_['entry_show_header_language']     = 'Show Language';
 $_['entry_show_footer']              = 'Show Footer';
-$_['entry_choose_products']          = 'Selected Feed Item';
 $_['entry_show_product_description'] = 'Show Product Description';
 $_['entry_show_product_price']       = 'Show Product Price';
 $_['entry_show_product_rating']      = 'Show Product Rating';
 $_['entry_show_addtocart']           = 'Show Product AddtoCart Button';
 $_['entry_status']     		         = 'Status';
-$_['entry_data_feed']                = 'Generated code';
+$_['entry_data_feed']                = 'Generated Code';
 
 // Help
-$_['help_ssl']  			         = 'This Feed just work SSL active, Firstly please active SSL then use it!';
-$_['help_data_feed']  		         = 'just copy this code';
+$_['help_ssl']  			         = 'This feed works only with SSL enabled!';
+$_['help_data_feed']  		         = 'Just copy-paste this code to Facebook.';
 
 // Error
-$_['error_permission']               = 'Warning: You do not have permission to modify module facebook store!';
-$_['error_app_id']                   = 'Facebook App ID - required!';
+$_['error_permission']               = 'Warning: You do not have permission to modify Facebook Store feed!';
+$_['error_app_id']                   = 'Facebook App ID is required!';
