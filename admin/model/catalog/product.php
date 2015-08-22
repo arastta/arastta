a3117ba2a7bc1ca091cfaab2e7dbccc84c75acf5
Valdeir Santana <wallace_gatinho_cat@hotmail.com>
2015-08-22 19:04:04 -0300

    Undefined index: tag
    This error occurs when trying to edit a product. It is at version 1.1.0 "Stable"
    
    Notice: Undefined index: tag in /home/XXXX/public_html/arastta_test/admin/model/catalog/product.php on line 155
    Notice: Undefined index: tag in /home/XXXX/public_html/arastta_test/admin/model/catalog/product.php on line 157
    Warning: Cannot modify header information - headers already sent by (output started at /home/XXXX/public_html/arastta_test/system/library/app.php:86) in /home/XXXX/public_html/arastta_test/system/library/response.php on line 23

--- admin/model/catalog/product.php
+++ admin/model/catalog/product.php
@@ -151,7 +151,11 @@ class ModelCatalogProduct extends Model {
 
 		foreach ($data['product_description'] as $language_id => $value) {
 			empty($value['meta_title']) ? $value['meta_title'] = $value['name'] : $value['meta_title'];
-            !empty($value['tag']) ? $value['tag'] = implode(',', $value['tag']) : $value['tag'];
+            		if (isset($value['tag'])) {
+				!empty($value['tag']) ? $value['tag'] = implode(',', $value['tag']) : $value['tag'];
+			} else {
+				$value['tag'] = '';
+			}
 
 			$this->db->query("INSERT INTO " . DB_PREFIX . "product_description SET product_id = '" . (int)$product_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', description = '" . $this->db->escape($value['description']) . "', tag = '" . $this->db->escape($value['tag']) . "', meta_title = '" . $this->db->escape($value['meta_title']) . "', meta_description = '" . $this->db->escape($value['meta_description']) . "', meta_keyword = '" . $this->db->escape($value['meta_keyword']) . "'");
 		}
