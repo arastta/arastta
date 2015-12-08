e5e98ff991b9e9950a35c1e6d76d6d55edcaf648
Burak Çakırel <burakcakirel@gmail.com>
2015-12-08 15:56:30 +0200

    Not aligned "copyright" line in license

--- system/engine/controller.php
+++ system/engine/controller.php
@@ -1,12 +1,13 @@
 <?php
 /**
  * @package        Arastta eCommerce
- * @copyright    Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
  * @credits        See CREDITS.txt for credits and other copyright notices.
  * @license        GNU General Public License version 3; see LICENSE.txt
  */
 
-abstract class Controller {    
+abstract class Controller {
+    
     protected $registry;
 
     public function __construct($registry) {
