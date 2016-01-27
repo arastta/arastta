533d05884a44591e030adebf769be4ada5033852
Denis Dulici <denisdulici@gmail.com>
2016-01-27 14:32:59 +0200

    install app

--- install/index.php
+++ install/index.php
@@ -16,91 +16,21 @@ if (version_compare(PHP_VERSION, '5.3.10', '<')) {
 define('AREXE', 1);
 
 require_once('define.php');
-require_once(DIR_SYSTEM . 'library/client.php');
-
-Client::setName('install');
 
 // Startup
+require_once(DIR_SYSTEM . 'library/client.php');
+Client::setName('install');
+
 require_once(DIR_SYSTEM . 'startup.php');
 
-// Registry
-$registry = new Registry();
+// App
+$app = new Install();
 
-// File System
-$filesystem = new Filesystem();
-$registry->set('filesystem', $filesystem);
+// Initialise main classes
+$app->initialise();
 
-// Loader
-$loader = new Loader($registry);
-$registry->set('load', $loader);
+// Dispatch the app
+$app->dispatch();
 
-// Trigger
-$trigger = new Trigger($registry);
-$registry->set('trigger', $trigger);
-
-// Url
-$url = new Url(HTTP_SERVER, HTTPS_SERVER, $registry);
-$registry->set('url', $url);
-
-// Uri
-$uri = new Uri();
-$registry->set('uri', $uri);
-
-// Security
-$security = new Security($registry);
-$registry->set('security', $security);
-
-// Request
-$request = new Request($registry);
-$registry->set('request', $request);
-
-// Response
-$response = new Response();
-$response->addHeader('Content-Type: text/html; charset=UTF-8');
-$registry->set('response', $response);
-
-// Document
-$document = new Document();
-$registry->set('document', $document);
-
-// Session
-$session = new Session();
-$registry->set('session', $session);
-
-// Utility
-$utility = new Utility($registry);
-$registry->set('utility', $utility);
-
-// Language
-if (isset($request->get['lang']) && $filesystem->exists(DIR_LANGUAGE . $request->get['lang'])) {
-    $lang = $request->get['lang'];
-    $route = 'main';
-} else {
-    $lang = 'en-GB';
-    $request->get['lang'] = $lang;
-    $route = 'language';
-}
-
-$language = new Language($lang, $registry);
-$registry->set('language', $language);
-
-// Config
-if (file_exists(DIR_ROOT . 'config.php') && filesize(DIR_ROOT . 'config.php') > 0) {
-    require_once(DIR_ROOT . 'config.php');
-}
-
-// Front Controller
-$controller = new Front($registry);
-
-// Router
-if (isset($request->get['route'])) {
-    $action = new Action($request->get['route']);
-} else {
-    $action = new Action($route);
-}
-
-// Dispatch
-$controller->dispatch($action, new Action('not_found'));
-
-// Output
-$response->output();
+// Render the output
+$app->render();
---
+++ system/library/app/install.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @package        Arastta eCommerce
+ * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @credits        See CREDITS.txt for credits and other copyright notices.
+ * @license        GNU General Public License version 3; see LICENSE.txt
+ */
+
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
+
+class Install extends App
+{
+
+    protected $_route = 'main';
+
+    public function initialise()
+    {
+        // File System
+        $this->registry->set('filesystem', new Filesystem());
+
+        // Loader
+        $this->registry->set('load', new Loader($this->registry));
+
+        // Trigger
+        $this->registry->set('trigger', new Trigger($this->registry));
+
+        // Url
+        $this->registry->set('url', new Url(HTTP_SERVER, HTTPS_SERVER, $this->registry));
+
+        // Uri
+        $this->registry->set('uri', new Uri());
+
+        // Security
+        $this->registry->set('security', new Security($this->registry));
+
+        // Request
+        $this->registry->set('request', new Request($this->registry));
+
+        // Response
+        $response = new Response();
+        $response->addHeader('Content-Type: text/html; charset=utf-8');
+        $this->registry->set('response', $response);
+
+        // Session
+        $this->registry->set('session', new Session());
+
+        // Utility
+        $utility = new Utility($this->registry);
+        $this->registry->set('utility', $utility);
+
+        // Language
+        if (isset($this->request->get['lang']) && $this->filesystem->exists(DIR_LANGUAGE . $this->request->get['lang'])) {
+            $lang = $this->request->get['lang'];
+        } else {
+            $lang = 'en-GB';
+            $this->request->get['lang'] = $lang;
+            $this->_route = 'language';
+        }
+
+        $language = new Language($lang, $this->registry);
+        $this->registry->set('language', $language);
+
+        // Document
+        $this->registry->set('document', new Document());
+        
+        $this->trigger->fire('post.app.initialise');
+    }
+
+    public function dispatch()
+    {
+        // Front Controller
+        $controller = new Front($this->registry);
+
+        // Router
+        if (isset($this->request->get['route'])) {
+            $action = new Action($this->request->get['route']);
+        } else {
+            $action = new Action($this->_route);
+        }
+
+        // Dispatch
+        $controller->dispatch($action, new Action('error/not_found'));
+
+        $this->trigger->fire('post.app.dispatch');
+    }
+
+    public function render()
+    {
+        // Render
+        $this->response->output();
+
+        $this->trigger->fire('post.app.render');
+    }
+}
