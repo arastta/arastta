ec7986335451890e468150528d12f92a10dbff38
Matt <mattythebatty@gmail.com>
2016-01-31 23:52:51 +0000

    add maintenance commands to enable and disable the store

---
+++ system/library/command/down.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Command;
+
+class Down extends Command {
+
+    protected $name = 'down';
+
+    protected $description = 'Put the store into maintenance mode';
+
+    public function fire()
+    {
+        $this->loadAdminModel('setting/setting');
+
+        $this->admin->model_setting_setting->editSettingValue('config', 'config_maintenance', 1);
+
+        $this->info('Maintenance mode enabled');
+    }
+
+}
---
+++ system/library/command/up.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Command;
+
+class Up extends Command {
+
+    protected $name = 'up';
+
+    protected $description = 'Take the store out of maintenance mode';
+
+    public function fire()
+    {
+        $this->loadAdminModel('setting/setting');
+
+        $this->admin->model_setting_setting->editSettingValue('config', 'config_maintenance', 0);
+
+        $this->info('Maintenance mode disabled');
+    }
+
+}
