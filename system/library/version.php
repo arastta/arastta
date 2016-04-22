ed6a9d4554c66ad266d889857546087da2f361b7
cuneytsenturk <cneytsenturk@gmail.com>
2016-04-22 16:40:04 +0200

    version update 1.3.0 RC 1 to 1.3.0 RC 2

--- system/library/version.php
+++ system/library/version.php
@@ -28,11 +28,11 @@ final class Version extends Object
 
     protected $build = '';
 
-    protected $status = 'RC 1';
+    protected $status = 'RC 2';
 
-    protected $date = '19-April-2016';
+    protected $date = '22-April-2016';
 
-    protected $time = '15:00';
+    protected $time = '17:00';
 
     protected $zone = 'GMT +2';
 
