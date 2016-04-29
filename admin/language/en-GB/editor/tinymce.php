034d083986cfd51a5c06e778e2e49fbb14c4559a
denisdulici <denisdulici@gmail.com>
2016-04-29 14:27:42 +0300

    language strings

--- admin/language/en-GB/editor/tinymce.php
+++ admin/language/en-GB/editor/tinymce.php
@@ -7,19 +7,19 @@
  */
 
 // Heading
-$_['heading_title'] = 'TinyMCE';
+$_['heading_title']                     = 'TinyMCE';
 
 // Text
-$_['text_editor']  = 'Editor';
-$_['text_success'] = 'Success: You have modified TinyMCE editor!';
-$_['text_edit']    = 'Edit TinyMCE';
-$_['text_menu']    = 'Menu';
-$_['text_tool']    = 'Toolbar';
+$_['text_editor']                       = 'Editor';
+$_['text_success']                      = 'Success: You have modified TinyMCE editor!';
+$_['text_edit']                         = 'Edit TinyMCE';
+$_['text_menu']                         = 'Menu';
+$_['text_tool']                         = 'Toolbar';
 
 // Entry
 $_['entry_height']                       = 'Height';
 $_['entry_menubar']                      = 'Menubar';
-$_['entry_menu_edit_insertfile']         = 'Insert File';
+$_['entry_menu_edit_insertfile']         = 'Insert file';
 $_['entry_menu_edit_undo']               = 'Undo';
 $_['entry_menu_edit_redo']               = 'Redo';
 $_['entry_menu_styleselect_styleselect'] = 'Style select';
@@ -30,53 +30,53 @@ $_['entry_menu_format_visualchars']      = 'Invisible characters';
 $_['entry_menu_format_visualblocks']     = 'Blocks';
 $_['entry_menu_format_ltr']              = 'Left to right';
 $_['entry_menu_format_rtl']              = 'Right to left';
-$_['entry_menu_format_fontselect']       = 'Font Family';
-$_['entry_menu_format_fontsizeselect']   = 'Font Size';
-$_['entry_menu_format_charmap']          = 'Special Characters';
+$_['entry_menu_format_fontselect']       = 'Font family';
+$_['entry_menu_format_fontsizeselect']   = 'Font size';
+$_['entry_menu_format_charmap']          = 'Special characters';
 $_['entry_menu_format_forecolor']        = 'Text color';
 $_['entry_menu_format_backcolor']        = 'Background color';
-$_['entry_menu_view_alignleft']          = 'Align Left';
-$_['entry_menu_view_aligncenter']        = 'Align Center';
-$_['entry_menu_view_alignright']         = 'Align Right';
+$_['entry_menu_view_alignleft']          = 'Align left';
+$_['entry_menu_view_aligncenter']        = 'Align center';
+$_['entry_menu_view_alignright']         = 'Align right';
 $_['entry_menu_view_alignjustify']       = 'Justify';
-$_['entry_menu_file_bullist']            = 'Bullet List';
-$_['entry_menu_file_numlist']            = 'Numbered List';
-$_['entry_menu_file_outdent']            = 'Decrease Indent';
-$_['entry_menu_file_indent']             = 'Increase Indent';
+$_['entry_menu_file_bullist']            = 'Bullet list';
+$_['entry_menu_file_numlist']            = 'Numbered list';
+$_['entry_menu_file_outdent']            = 'Decrease indent';
+$_['entry_menu_file_indent']             = 'Increase indent';
 $_['entry_menu_file_table']              = 'Right to left';
-$_['entry_menu_insert_link']             = 'Insert/edit Link';
-$_['entry_menu_insert_image']            = 'Insert/edit Image';
-$_['entry_menu_insert_media']            = 'Insert/edit Video';
+$_['entry_menu_insert_link']             = 'Insert/edit link';
+$_['entry_menu_insert_image']            = 'Insert/edit image';
+$_['entry_menu_insert_media']            = 'Insert/edit video';
 $_['entry_menu_insert_hr']               = 'Horizontal line';
-$_['entry_menu_tools_fullscreen']        = 'Full-secreen';
+$_['entry_menu_tools_fullscreen']        = 'Full screen';
 $_['entry_menu_tools_code']              = 'Source code';
 $_['entry_menu_tools_searchreplace']     = 'Find and replace';
 $_['entry_menu_tools_pagebreak']         = 'Page break';
 $_['entry_menu_tools_nonbreaking']       = 'Non-breaking space';
 $_['entry_menu_tools_emoticons']         = 'Emoticons';
 $_['entry_status']                       = 'Status';
-$_['entry_sort_order']                   = 'Sort Order';
+$_['entry_sort_order']                   = 'Sort order';
 
 // Tab
 $_['tab_advanced'] = 'Advanced';
 
 // Help
-$_['help_height']                 = 'Editor height.';
-$_['help_menu_edit_undo']         = 'TinyMCE Editor Undo button.';
-$_['help_menu_edit_redo']         = 'TinyMCE Editor Redo button.';
-$_['help_menu_format_bold']       = 'TinyMCE Editor Bold button.';
-$_['help_menu_format_italic']     = 'TinyMCE Editor Italic button.';
-$_['help_menu_view_alignleft']    = 'TinyMCE Editor Align Left button.';
-$_['help_menu_view_aligncenter']  = 'TinyMCE Editor Align Center button.';
-$_['help_menu_view_alignright']   = 'TinyMCE Editor Align Right button.';
-$_['help_menu_view_alignjustify'] = 'TinyMCE Editor Justify button.';
-$_['help_menu_file_bullist']      = 'TinyMCE Editor Bullet List button.';
-$_['help_menu_file_numlist']      = 'TinyMCE Editor Numbered List button.';
-$_['help_menu_file_outdent']      = 'TinyMCE Editor Decrease Indent button.';
-$_['help_menu_file_indent']       = 'TinyMCE Editor Increase Indent button.';
-$_['help_menu_insert_link']       = 'TinyMCE Editor Insert/edit Link button.';
-$_['help_menu_insert_image']      = 'TinyMCE Editor Insert/edit Image button.';
-$_['help_menu_tools_imagetools']  = 'TinyMCE Editor Emoticons button.';
+$_['help_height']                       = 'Editor height.';
+$_['help_menu_edit_undo']               = 'Show Undo button.';
+$_['help_menu_edit_redo']               = 'Show Redo button.';
+$_['help_menu_format_bold']             = 'Show Bold button.';
+$_['help_menu_format_italic']           = 'Show Italic button.';
+$_['help_menu_view_alignleft']          = 'Show Align Left button.';
+$_['help_menu_view_aligncenter']        = 'Show Align Center button.';
+$_['help_menu_view_alignright']         = 'Show Align Right button.';
+$_['help_menu_view_alignjustify']       = 'Show Justify button.';
+$_['help_menu_file_bullist']            = 'Show Bullet List button.';
+$_['help_menu_file_numlist']            = 'Show Numbered List button.';
+$_['help_menu_file_outdent']            = 'Show Decrease Indent button.';
+$_['help_menu_file_indent']             = 'Show Increase Indent button.';
+$_['help_menu_insert_link']             = 'Show Insert/edit Link button.';
+$_['help_menu_insert_image']            = 'Show Insert/edit Image button.';
+$_['help_menu_tools_imagetools']        = 'Show Emoticons button.';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify editor TinyMCE!';
+$_['error_permission']                  = 'Warning: You do not have permission to modify editor TinyMCE!';
