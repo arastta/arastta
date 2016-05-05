c8f357a9ee2a04b5883663dd974d286e2a82238b
cuneytsenturk <cneytsenturk@gmail.com>
2016-05-05 15:37:23 +0300

    close #456 Fixed: Search button not working in Second theme

--- catalog/view/javascript/common.js
+++ catalog/view/javascript/common.js
@@ -73,7 +73,7 @@ $(document).ready(function() {
     $('#search input[name=\'search\']').parent().find('button').on('click', function() {
         url = $('base').attr('href') + 'index.php?route=product/search';
 
-        var value = $('header input[name=\'search\']').val();
+        var value = $('#search input[name=\'search\']').val();
 
         if (value) {
             url += '&search=' + encodeURIComponent(value);
@@ -84,7 +84,7 @@ $(document).ready(function() {
 
     $('#search input[name=\'search\']').on('keydown', function(e) {
         if (e.keyCode == 13) {
-            $('header input[name=\'search\']').parent().find('button').trigger('click');
+            $(this).parent().find('button').trigger('click');
         }
     });
 
