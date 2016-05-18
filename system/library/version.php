a4ce72b283bd7ab395218d22f39ecae7907fcb13
cuneytsenturk <cneytsenturk@gmail.com>
2016-05-18 16:03:48 +0300

    version update 1.3.1 to 1.3.2

--- system/library/version.php
+++ system/library/version.php
@@ -24,15 +24,15 @@ final class Version extends Object
 
     protected $minor = '3';
 
-    protected $patch = '1';
+    protected $patch = '2';
 
     protected $build = '';
 
     protected $status = 'Stable';
 
-    protected $date = '29-April-2016';
+    protected $date = '18-May-2016';
 
-    protected $time = '14:00';
+    protected $time = '16:00';
 
     protected $zone = 'GMT +2';
 
