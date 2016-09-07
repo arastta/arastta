2ada9f1aa0148e4990a3c91733b4b1e44aeed467
cuneytsenturk <cneytsenturk@gmail.com>
2016-09-07 10:38:10 +0300

    close #547 Fixed: Localisation => Location sorting issue

--- admin/controller/localisation/location.php
+++ admin/controller/localisation/location.php
@@ -137,7 +137,7 @@ class ControllerLocalisationLocation extends Controller {
         if (isset($this->request->get['sort'])) {
             $sort = $this->request->get['sort'];
         } else {
-            $sort = 'l.name';
+            $sort = 'name';
         }
 
         if (isset($this->request->get['order'])) {
