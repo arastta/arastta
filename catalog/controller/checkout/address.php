c1c1c9945ff56363fb8914e111476ff9923b54eb
syntaxerror <syntaxerror.no@gmail.com>
2016-11-12 20:20:34 +0200

    Register Checkout - Remember firstname and lastname on address step

--- catalog/controller/checkout/address.php
+++ catalog/controller/checkout/address.php
@@ -369,6 +369,8 @@ class ControllerCheckoutAddress extends Controller
 
         if (isset($this->session->data['payment_address']['firstname'])) {
             $data['payment_firstname'] = $this->session->data['payment_address']['firstname'];
+        } else if (isset($this->session->data['customer_id'])) {
+            $data['payment_firstname'] = $this->customer->getFirstName();
         } else if (isset($this->session->data['guest']['firstname'])) {
             $data['payment_firstname'] = $this->session->data['guest']['firstname'];
         } else {
@@ -377,6 +379,8 @@ class ControllerCheckoutAddress extends Controller
 
         if (isset($this->session->data['payment_address']['lastname'])) {
             $data['payment_lastname'] = $this->session->data['payment_address']['lastname'];
+        } else if (isset($this->session->data['customer_id'])) {
+            $data['payment_lastname'] = $this->customer->getLastName();
         } else if (isset($this->session->data['guest']['lastname'])) {
             $data['payment_lastname'] = $this->session->data['guest']['lastname'];
         } else {
