7ce7884416ddbefb4472c3ae17884a0ab4f6c972
syntaxerror <syntaxerror.no@gmail.com>
2016-11-24 10:41:27 +0200

    Use the new cache manager added in 1.5

--- admin/language/en-GB/tool/backup.php
+++ admin/language/en-GB/tool/backup.php
@@ -11,7 +11,7 @@ $_['heading_title']    = 'Backup &amp; Restore';
 
 // Text
 $_['text_backup']      = 'Download Backup';
-$_['text_success']     = 'Success: You have successfully imported your database!<br />Note! We recommend you clear the cache under "System -> Settings -> Cache" to ensure all is loaded correctly.';
+$_['text_success']     = 'Success: You have successfully imported your database!<br />Note! We recommend you clear the cached content under "Tools -> Cache Manager" to ensure all is loaded correctly.';
 $_['text_list']        = 'Upload List';
 
 // Entry
