3bf3df723d45d24ab53b90a808b175f75e79db1e
cuneytsenturk <cneytsenturk@gmail.com>
2017-03-01 12:20:22 +0300

    close #666 Fixed: Products Module - Displaying unpublished products

--- catalog/model/module/products.php
+++ catalog/model/module/products.php
@@ -12,75 +12,69 @@ class ModelModuleProducts extends Model
 
     public function getBestSellerProducts($data = array())
     {
-        $product_data = $this->cache->get('product.bestseller.' . (int) $this->config->get('config_language_id') . '.' . (int) $this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . json_encode($data['categories']) . '.' . (int) $data['limit']);
+        $product_data = array();
 
-        if (!$product_data) {
-            $product_data = array();
+        $sql = "SELECT op.product_id, SUM(op.quantity) AS total FROM " . DB_PREFIX . "order_product op LEFT JOIN `" . DB_PREFIX . "order` o ON (op.order_id = o.order_id) LEFT JOIN `" . DB_PREFIX . "product` p ON (op.product_id = p.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id)";
 
-            $sql = "SELECT op.product_id, SUM(op.quantity) AS total FROM " . DB_PREFIX . "order_product op LEFT JOIN `" . DB_PREFIX . "order` o ON (op.order_id = o.order_id) LEFT JOIN `" . DB_PREFIX . "product` p ON (op.product_id = p.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id)";
+        if (!empty($data['categories'])) {
+            $sql .= " LEFT JOIN " . DB_PREFIX . "product_to_category p2c ON (p.product_id = p2c.product_id)";
+        }
 
-            if (!empty($data['categories'])) {
-                $sql .= " LEFT JOIN " . DB_PREFIX . "product_to_category p2c ON (p.product_id = p2c.product_id)";
-            }
+        $sql .= " WHERE o.order_status_id > '0' AND p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int) $this->config->get('config_store_id') . "'";
 
-            $sql .= " WHERE o.order_status_id > '0' AND p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int) $this->config->get('config_store_id') . "'";
+        if (!empty($data['categories'])) {
+            $sql .= " AND p2c.category_id IN (" . implode(',', $data['categories']) . ")";
+        }
 
-            if (!empty($data['categories'])) {
-                $sql .= " AND p2c.category_id IN (" . implode(',', $data['categories']) . ")";
-            }
+        $sql .= " GROUP BY op.product_id";
 
-            $sql .= " GROUP BY op.product_id";
+        $sort_data = array(
+            'total',
+            'pd.name',
+            'p.model',
+            'p.quantity',
+            'p.price',
+            'rating',
+            'p.sort_order',
+            'p.date_added'
+        );
 
-            $sort_data = array(
-                'total',
-                'pd.name',
-                'p.model',
-                'p.quantity',
-                'p.price',
-                'rating',
-                'p.sort_order',
-                'p.date_added'
-            );
-
-            if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
-                if ($data['sort'] == 'pd.name' || $data['sort'] == 'p.model') {
-                    $sql .= " ORDER BY LCASE(" . $data['sort'] . ")";
-                } elseif ($data['sort'] == 'p.price') {
-                    $sql .= " ORDER BY (CASE WHEN special IS NOT NULL THEN special WHEN discount IS NOT NULL THEN discount ELSE p.price END)";
-                } else {
-                    $sql .= " ORDER BY " . $data['sort'];
-                }
+        if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+            if ($data['sort'] == 'pd.name' || $data['sort'] == 'p.model') {
+                $sql .= " ORDER BY LCASE(" . $data['sort'] . ")";
+            } elseif ($data['sort'] == 'p.price') {
+                $sql .= " ORDER BY (CASE WHEN special IS NOT NULL THEN special WHEN discount IS NOT NULL THEN discount ELSE p.price END)";
             } else {
-                $sql .= " ORDER BY p.sort_order";
+                $sql .= " ORDER BY " . $data['sort'];
+            }
+        } else {
+            $sql .= " ORDER BY p.sort_order";
+        }
+
+        if (isset($data['order']) && ($data['order'] == 'DESC')) {
+            $sql .= " DESC, LCASE(pd.name) DESC";
+        } else {
+            $sql .= " ASC, LCASE(pd.name) ASC";
+        }
+
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) {
+                $data['start'] = 0;
             }
 
-            if (isset($data['order']) && ($data['order'] == 'DESC')) {
-                $sql .= " DESC, LCASE(pd.name) DESC";
-            } else {
-                $sql .= " ASC, LCASE(pd.name) ASC";
+            if ($data['limit'] < 1) {
+                $data['limit'] = 20;
             }
 
-            if (isset($data['start']) || isset($data['limit'])) {
-                if ($data['start'] < 0) {
-                    $data['start'] = 0;
-                }
+            $sql .= " LIMIT " . (int) $data['start'] . "," . (int) $data['limit'];
+        }
 
-                if ($data['limit'] < 1) {
-                    $data['limit'] = 20;
-                }
+        $query = $this->db->query($sql);
 
-                $sql .= " LIMIT " . (int) $data['start'] . "," . (int) $data['limit'];
-            }
+        $this->load->model('catalog/product');
 
-            $query = $this->db->query($sql);
-
-            $this->load->model('catalog/product');
-
-            foreach ($query->rows as $result) {
-                $product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
-            }
-
-            $this->cache->set('product.bestseller.' . (int) $this->config->get('config_language_id') . '.' . (int) $this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . json_encode($data['categories']) . '.' . (int) $data['limit'], $product_data);
+        foreach ($query->rows as $result) {
+            $product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
         }
 
         return $product_data;
@@ -88,72 +82,66 @@ class ModelModuleProducts extends Model
 
     public function getLatestProducts($data = array())
     {
-        $product_data = $this->cache->get('product.latest.' . (int) $this->config->get('config_language_id') . '.' . (int) $this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . json_encode($data['categories']) . '.' . (int) $data['limit']);
+        $sql = "SELECT p.product_id FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id)";
 
-        if (!$product_data) {
-            $sql = "SELECT p.product_id FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id)";
+        if (!empty($data['categories'])) {
+            $sql .= " LEFT JOIN " . DB_PREFIX . "product_to_category p2c ON (p.product_id = p2c.product_id)";
+        }
 
-            if (!empty($data['categories'])) {
-                $sql .= " LEFT JOIN " . DB_PREFIX . "product_to_category p2c ON (p.product_id = p2c.product_id)";
-            }
+        $sql .=" WHERE p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int) $this->config->get('config_store_id') . "'";
 
-            $sql .=" WHERE p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int) $this->config->get('config_store_id') . "'";
+        if (!empty($data['categories'])) {
+            $sql .= " AND p2c.category_id IN (" . implode(',', $data['categories']) . ")";
+        }
 
-            if (!empty($data['categories'])) {
-                $sql .= " AND p2c.category_id IN (" . implode(',', $data['categories']) . ")";
-            }
+        $sql .= " GROUP BY p.product_id";
 
-            $sql .= " GROUP BY p.product_id";
+        $sort_data = array(
+            'pd.name',
+            'p.model',
+            'p.quantity',
+            'p.price',
+            'rating',
+            'p.sort_order',
+            'p.date_added'
+        );
 
-            $sort_data = array(
-                'pd.name',
-                'p.model',
-                'p.quantity',
-                'p.price',
-                'rating',
-                'p.sort_order',
-                'p.date_added'
-            );
-
-            if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
-                if ($data['sort'] == 'pd.name' || $data['sort'] == 'p.model') {
-                    $sql .= " ORDER BY LCASE(" . $data['sort'] . ")";
-                } elseif ($data['sort'] == 'p.price') {
-                    $sql .= " ORDER BY (CASE WHEN special IS NOT NULL THEN special WHEN discount IS NOT NULL THEN discount ELSE p.price END)";
-                } else {
-                    $sql .= " ORDER BY " . $data['sort'];
-                }
+        if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+            if ($data['sort'] == 'pd.name' || $data['sort'] == 'p.model') {
+                $sql .= " ORDER BY LCASE(" . $data['sort'] . ")";
+            } elseif ($data['sort'] == 'p.price') {
+                $sql .= " ORDER BY (CASE WHEN special IS NOT NULL THEN special WHEN discount IS NOT NULL THEN discount ELSE p.price END)";
             } else {
-                $sql .= " ORDER BY p.sort_order";
+                $sql .= " ORDER BY " . $data['sort'];
+            }
+        } else {
+            $sql .= " ORDER BY p.sort_order";
+        }
+
+        if (isset($data['order']) && ($data['order'] == 'DESC')) {
+            $sql .= " DESC, LCASE(pd.name) DESC";
+        } else {
+            $sql .= " ASC, LCASE(pd.name) ASC";
+        }
+
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) {
+                $data['start'] = 0;
             }
 
-            if (isset($data['order']) && ($data['order'] == 'DESC')) {
-                $sql .= " DESC, LCASE(pd.name) DESC";
-            } else {
-                $sql .= " ASC, LCASE(pd.name) ASC";
+            if ($data['limit'] < 1) {
+                $data['limit'] = 20;
             }
 
-            if (isset($data['start']) || isset($data['limit'])) {
-                if ($data['start'] < 0) {
-                    $data['start'] = 0;
-                }
+            $sql .= " LIMIT " . (int) $data['start'] . "," . (int) $data['limit'];
+        }
 
-                if ($data['limit'] < 1) {
-                    $data['limit'] = 20;
-                }
+        $query = $this->db->query($sql);
 
-                $sql .= " LIMIT " . (int) $data['start'] . "," . (int) $data['limit'];
-            }
+        $this->load->model('catalog/product');
 
-            $query = $this->db->query($sql);
-
-            $this->load->model('catalog/product');
-
-            foreach ($query->rows as $result) {
-                $product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
-            }
-
-            $this->cache->set('product.latest.' . (int) $this->config->get('config_language_id') . '.' . (int) $this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . json_encode($data['categories']) . '.' . (int) $data['limit'], $product_data);
+        foreach ($query->rows as $result) {
+            $product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
         }
 
         return $product_data;
@@ -161,70 +149,64 @@ class ModelModuleProducts extends Model
 
     public function getProductSpecials($data = array())
     {
-        $product_data = $this->cache->get('product.special.' . (int) $this->config->get('config_language_id') . '.' . (int) $this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . json_encode($data['categories']) . '.' . (int) $data['limit']);
+        $sql = "SELECT DISTINCT ps.product_id, (SELECT AVG(rating) FROM " . DB_PREFIX . "review r1 WHERE r1.product_id = ps.product_id AND r1.status = '1' GROUP BY r1.product_id) AS rating FROM " . DB_PREFIX . "product_special ps LEFT JOIN " . DB_PREFIX . "product p ON (ps.product_id = p.product_id) LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) ";
 
-        if (!$product_data) {
-            $sql = "SELECT DISTINCT ps.product_id, (SELECT AVG(rating) FROM " . DB_PREFIX . "review r1 WHERE r1.product_id = ps.product_id AND r1.status = '1' GROUP BY r1.product_id) AS rating FROM " . DB_PREFIX . "product_special ps LEFT JOIN " . DB_PREFIX . "product p ON (ps.product_id = p.product_id) LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) ";
+        if (!empty($data['categories'])) {
+            $sql .= " LEFT JOIN " . DB_PREFIX . "product_to_category p2c ON (p.product_id = p2c.product_id)";
+        }
 
-            if (!empty($data['categories'])) {
-                $sql .= " LEFT JOIN " . DB_PREFIX . "product_to_category p2c ON (p.product_id = p2c.product_id)";
-            }
+        $sql .= " WHERE p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int) $this->config->get('config_store_id') . "' AND ps.customer_group_id = '" . (int) $this->config->get('config_customer_group_id') . "' AND ((ps.date_start = '0000-00-00' OR ps.date_start < NOW()) AND (ps.date_end = '0000-00-00' OR ps.date_end > NOW()))";
 
-            $sql .= " WHERE p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int) $this->config->get('config_store_id') . "' AND ps.customer_group_id = '" . (int) $this->config->get('config_customer_group_id') . "' AND ((ps.date_start = '0000-00-00' OR ps.date_start < NOW()) AND (ps.date_end = '0000-00-00' OR ps.date_end > NOW()))";
+        if (!empty($data['categories'])) {
+            $sql .= " AND p2c.category_id IN (" . implode(',', $data['categories']) . ")";
+        }
 
-            if (!empty($data['categories'])) {
-                $sql .= " AND p2c.category_id IN (" . implode(',', $data['categories']) . ")";
-            }
+        $sql .= " GROUP BY ps.product_id";
 
-            $sql .= " GROUP BY ps.product_id";
+        $sort_data = array(
+            'pd.name',
+            'p.model',
+            'ps.price',
+            'rating',
+            'p.sort_order'
+        );
 
-            $sort_data = array(
-                'pd.name',
-                'p.model',
-                'ps.price',
-                'rating',
-                'p.sort_order'
-            );
-
-            if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
-                if ($data['sort'] == 'pd.name' || $data['sort'] == 'p.model') {
-                    $sql .= " ORDER BY LCASE(" . $data['sort'] . ")";
-                } else {
-                    $sql .= " ORDER BY " . $data['sort'];
-                }
+        if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+            if ($data['sort'] == 'pd.name' || $data['sort'] == 'p.model') {
+                $sql .= " ORDER BY LCASE(" . $data['sort'] . ")";
             } else {
-                $sql .= " ORDER BY p.sort_order";
+                $sql .= " ORDER BY " . $data['sort'];
+            }
+        } else {
+            $sql .= " ORDER BY p.sort_order";
+        }
+
+        if (isset($data['order']) && ($data['order'] == 'DESC')) {
+            $sql .= " DESC, LCASE(pd.name) DESC";
+        } else {
+            $sql .= " ASC, LCASE(pd.name) ASC";
+        }
+
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) {
+                $data['start'] = 0;
             }
 
-            if (isset($data['order']) && ($data['order'] == 'DESC')) {
-                $sql .= " DESC, LCASE(pd.name) DESC";
-            } else {
-                $sql .= " ASC, LCASE(pd.name) ASC";
+            if ($data['limit'] < 1) {
+                $data['limit'] = 20;
             }
 
-            if (isset($data['start']) || isset($data['limit'])) {
-                if ($data['start'] < 0) {
-                    $data['start'] = 0;
-                }
+            $sql .= " LIMIT " . (int) $data['start'] . "," . (int) $data['limit'];
+        }
 
-                if ($data['limit'] < 1) {
-                    $data['limit'] = 20;
-                }
+        $product_data = array();
 
-                $sql .= " LIMIT " . (int) $data['start'] . "," . (int) $data['limit'];
-            }
+        $query = $this->db->query($sql);
 
-            $product_data = array();
+        $this->load->model('catalog/product');
 
-            $query = $this->db->query($sql);
-
-            $this->load->model('catalog/product');
-
-            foreach ($query->rows as $result) {
-                $product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
-            }
-
-            $this->cache->set('product.special.' . (int) $this->config->get('config_language_id') . '.' . (int) $this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . json_encode($data['categories']) . '.' . (int) $data['limit'], $product_data);
+        foreach ($query->rows as $result) {
+            $product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
         }
 
         return $product_data;
